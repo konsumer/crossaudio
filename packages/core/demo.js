4cbaa38daae1a6c085ed6451735cd29beaab7601
David Konsumer <konsumer@jetboystudio.com>
2021-07-15 07:36:51 -0700

    subscribe to events

--- packages/core/demo.js
+++ packages/core/demo.js
@@ -3,13 +3,30 @@
 import { StreamAudioContext as AudioContext } from 'web-audio-engine'
 import Speaker from 'speaker'
 import WebAudioScheduler from 'web-audio-scheduler'
+import emitonoff from 'emitonoff'
 
-const synth = (context) => {
-  const sched = new WebAudioScheduler({ context, timerAPI: global })
+const mysynth = (context, params) => {
+  function ticktack(e) {
+    // get value from params
+    console.log('ticktack:', params.cool)
+
+    const osc = context.createOscillator()
+    const amp = context.createGain()
+
+    const t0 = e.playbackTime
+    const t1 = t0 + e.args.duration
+    osc.frequency.value = e.args.frequency
+    osc.start(t0)
+    osc.stop(t1)
+    amp.gain.setValueAtTime(0.5 * e.args.amp, t0)
+    amp.gain.exponentialRampToValueAtTime(1e-6, t1)
+
+    osc.connect(amp)
+    amp.connect(context.destination)
+  }
 
   function metronome(e) {
     const t0 = e.playbackTime
-
     sched.insert(t0 + 0.0, ticktack, {
       frequency: 880,
       amp: 1.0,
@@ -33,27 +50,55 @@ const synth = (context) => {
     sched.insert(t0 + 2.0, metronome)
   }
 
-  function ticktack(e) {
-    const t0 = e.playbackTime
-    const t1 = t0 + e.args.duration
-    const osc = context.createOscillator()
-    const amp = context.createGain()
+  // subscribe to changes
+  params.on('cool', (val) => {
+    console.log('subscribe:', val)
+  })
 
-    osc.frequency.value = e.args.frequency
-    osc.start(t0)
-    osc.stop(t1)
-    osc.connect(amp)
-
-    amp.gain.setValueAtTime(0.5 * e.args.amp, t0)
-    amp.gain.exponentialRampToValueAtTime(1e-6, t1)
-    amp.connect(context.destination)
-  }
-
+  const sched = new WebAudioScheduler({ context, timerAPI: global })
   sched.start(metronome)
 }
 
-const context = new AudioContext()
-const speaker = new Speaker()
-context.pipe(speaker)
-context.resume()
-synth(context)
+// simple observable prop-store
+class Params {
+  constructor(values) {
+    this.values = values
+    this.watcher = emitonoff()
+    Object.keys(values).forEach((name) => {
+      Object.defineProperty(this, name, {
+        get: () => this.values[name],
+        set: (value) => {
+          this.values[name] = value
+          this.watcher.emit(name, value)
+        }
+      })
+    })
+  }
+
+  on(name, callback) {
+    this.watcher.on(name, callback)
+  }
+
+  off(name, callback) {
+    this.watcher.off(name, callback)
+  }
+}
+
+function play(synth, params) {
+  const context = new AudioContext()
+  const speaker = new Speaker()
+  context.pipe(speaker)
+  context.resume()
+  synth(context, params)
+}
+
+const params = new Params({
+  cool: true
+})
+
+// illustrates value changing
+setInterval(() => {
+  params.cool = !params.cool
+}, 1000)
+
+play(mysynth, params)
--- packages/core/index.js
+++ packages/core/index.js
@@ -1,23 +1,23 @@
-export { default as WebAudioScheduler } from "web-audio-scheduler";
+export { default as WebAudioScheduler } from 'web-audio-scheduler'
 
 // load an audio file into a context
 export const fetchAudioBuffer = async (context, filename) => {
   if (Array.isArray(filename)) {
     return Promise.all(
       filename.map((filename) => fetchAudioBuffer(context, filename))
-    );
+    )
   }
 
   if (process?.versions?.node) {
     const {
-      promises: { readFile },
-    } = await import("fs");
-    return context.decodeAudioData(await readFile(filename));
+      promises: { readFile }
+    } = await import('fs')
+    return context.decodeAudioData(await readFile(filename))
   } else {
-    return context.createMediaElementSource(new Audio(filename));
+    return context.createMediaElementSource(new Audio(filename))
   }
-};
+}
 
 // create a render-loop for audio
 // TODO: I'm not sure this will actually be the interface
-export const render = (synthCallback) => {};
+export const render = (synthCallback) => {}
