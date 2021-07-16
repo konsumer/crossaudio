29a7687547972c544c580eb850a3f878cdf22b7a
David Konsumer <konsumer@jetboystudio.com>
2021-07-16 16:03:57 -0700

    nicer dev for website with mdx

--- packages/crossaudio-site/src/main.jsx
+++ packages/crossaudio-site/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import App from './App.mdx'
 
 ReactDOM.render(
   <React.StrictMode>
--- packages/crossaudio-site/vite.config.js
+++ packages/crossaudio-site/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import mdx from 'vite-plugin-mdx'
 
-// https://vitejs.dev/config/
+const optionsRefresh = {}
+
+const optionsMdx = {
+  remarkPlugins: [],
+  rehypePlugins: []
+}
+
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh(optionsRefresh), mdx(optionsMdx)]
 })
