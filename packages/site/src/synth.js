7ac8d80f1d2739f0336cc201007650ba16bcec34
David Konsumer <konsumer@jetboystudio.com>
2021-07-17 03:34:03 -0700

    add lowpass filter to demo

--- packages/site/src/synth.js
+++ packages/site/src/synth.js
@@ -1,8 +1,29 @@
+// get midi frequency from 0-127
 const mtof = note => 440 * Math.pow(2, (note - 69) / 12)
+
+// get filter Q from 0-127: 0.0001 to 1000
+const mtoq = note => 0.0001 + ((note / 127) * 999.9999)
 
 export default function synth (context, params) {
   const vco = context.createOscillator()
   vco.frequency.value = mtof(params.note)
+
+  params.on('note', note => {
+    vco.frequency.value = mtof(note)
+  })
+
+  const vcf = context.createBiquadFilter()
+  vcf.type = 'lowpass'
+  vcf.frequency.value = mtof(params.cutoff)
+  vcf.Q.value = mtoq(params.resonance)
+
+  params.on('cutoff', cutoff => {
+    vcf.frequency.value = mtof(cutoff)
+  })
+
+  params.on('resonance', resonance => {
+    vcf.Q.value = mtoq(resonance)
+  })
 
   const vca = context.createGain()
   vca.gain.value = params.power ? 1 : 0
@@ -11,11 +32,8 @@ export default function synth (context, params) {
     vca.gain.value = power ? 1 : 0
   })
 
-  params.on('note', note => {
-    vco.frequency.value = mtof(note)
-  })
-
-  vco.connect(vca)
+  vco.connect(vcf)
+  vcf.connect(vca)
   vca.connect(context.destination)
   vco.start(0)
 }
