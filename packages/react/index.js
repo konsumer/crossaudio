f0985f2b2c7095f2d75d8bbfe54a686611625d25
David Konsumer <konsumer@jetboystudio.com>
2021-07-21 04:04:57 -0700

    easier Canvas in core for viz units

--- packages/react/index.js
+++ packages/react/index.js
@@ -5,6 +5,8 @@ export const context = createContext()
 
 export const useCrossAudio = () => useContext(context)
 
+// I am using React.createElement to avoid JSX compilation
+
 export const CrossAudioProvider = ({ synth, params, ...props }) => {
   const [state, setState] = useState(params)
   const actualParams = useRef(new Params(params))
@@ -20,6 +22,18 @@ export const CrossAudioProvider = ({ synth, params, ...props }) => {
     setState(value)
   }
 
-  // I do this so I don't need to transpile JSX
   return React.createElement(context.Provider, { ...props, value: [state, updateState] })
+}
+
+export const Canvas = ({ name = 'canvas', ...props }) => {
+  const ref = useRef()
+  const [params, setParams] = useCrossAudio()
+
+  useEffect(() => {
+    if (ref.current) {
+      setParams({ ...params, [name]: ref.current })
+    }
+  }, [ref.current])
+
+  return React.createElement('canvas', { ...props, ref })
 }
